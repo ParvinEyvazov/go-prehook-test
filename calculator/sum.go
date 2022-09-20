638d4c8502406e16b873f82a7e82032868f9cbb6
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2022-09-20 12:44:03 +0300

    first commit

---
+++ calculator/sum.go
@@ -0,0 +1,5 @@
+package calculator
+
+func Sum(a, b int) int {
+	return a + b
+}
---
+++ calculator/sum_test.go
@@ -0,0 +1,32 @@
+package calculator_test
+
+import (
+	"test/calculator"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+
+	t.Run("SUCCESS CASE", func(t *testing.T) {
+		a, b := 4, 5
+		expected := 9
+
+		result := calculator.Sum(a, b)
+
+		if expected != result {
+			t.Fatal("SUM ERROR [SUCCESS CASE]")
+		}
+	})
+
+	t.Run("ERROR CASE", func(t *testing.T) {
+		create_error := false
+
+		a, b := 4, 5
+		expected := 10
+		result := calculator.Sum(a, b)
+		if expected != result && create_error {
+			t.Fatal("SUM ERROR [ERROR CASE]")
+		}
+	})
+
+}
